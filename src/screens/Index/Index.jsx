626cbe7b307c278a59cd3059c100ca646916fbf6
dhanasekarbitsathy <95520638+dhanasekarbitsathy@users.noreply.github.com>
2023-10-04 11:50:40 +0530

    Initial commit

---
+++ src/screens/Index/Index.jsx
@@ -0,0 +1,308 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="overlap">
+          <div className="overlap-group">
+            <div className="scrolling-table">
+              <div className="column">
+                <div className="frame">
+                  <img
+                    className="vector"
+                    alt="Vector"
+                    src="https://cdn.animaapp.com/projects/651cfb9e15319872b1327744/releases/651cfbde0b3bcd1fa07481c6/img/vector@2x.png"
+                  />
+                  <div className="text-wrapper">Tac Id</div>
+                </div>
+                <div className="frame-2">
+                  <img
+                    className="bx-checkbox"
+                    alt="Bx checkbox"
+                    src="https://cdn.animaapp.com/projects/651cfb9e15319872b1327744/releases/651cfbde0b3bcd1fa07481c6/img/vector@2x.png"
+                  />
+                  <div className="text-wrapper-2">0001</div>
+                </div>
+                <div className="frame-2">
+                  <img
+                    className="img"
+                    alt="Bx checkbox"
+                    src="https://cdn.animaapp.com/projects/651cfb9e15319872b1327744/releases/651cfbde0b3bcd1fa07481c6/img/vector@2x.png"
+                  />
+                  <div className="text-wrapper-2">0002</div>
+                </div>
+                <div className="frame-2">
+                  <img
+                    className="bx-checkbox-2"
+                    alt="Bx checkbox"
+                    src="https://cdn.animaapp.com/projects/651cfb9e15319872b1327744/releases/651cfbde0b3bcd1fa07481c6/img/vector@2x.png"
+                  />
+                  <div className="text-wrapper-2">0003</div>
+                </div>
+                <div className="frame-2">
+                  <img
+                    className="bx-checkbox-3"
+                    alt="Bx checkbox"
+                    src="https://cdn.animaapp.com/projects/651cfb9e15319872b1327744/releases/651cfbde0b3bcd1fa07481c6/img/vector@2x.png"
+                  />
+                  <div className="text-wrapper-2">0004</div>
+                </div>
+                <div className="frame-2">
+                  <img
+                    className="bx-checkbox-4"
+                    alt="Bx checkbox"
+                    src="https://cdn.animaapp.com/projects/651cfb9e15319872b1327744/releases/651cfbde0b3bcd1fa07481c6/img/vector@2x.png"
+                  />
+                  <div className="text-wrapper-2">0005</div>
+                </div>
+                <div className="frame-2">
+                  <img
+                    className="bx-checkbox-5"
+                    alt="Bx checkbox"
+                    src="https://cdn.animaapp.com/projects/651cfb9e15319872b1327744/releases/651cfbde0b3bcd1fa07481c6/img/vector@2x.png"
+                  />
+                  <div className="text-wrapper-2">0006</div>
+                </div>
+              </div>
+              <div className="column-2">
+                <div className="frame">
+                  <div className="text-wrapper-3">Title</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Mask prediction</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Crowd prediction</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Emotion prediction</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Acid rain prediction</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Web page</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Acid rain prediction</div>
+                </div>
+              </div>
+              <div className="column-2">
+                <div className="frame">
+                  <div className="text-wrapper-3">Category</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Project/Product</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Project/Product</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Project/Product</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Project/Product</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Project/Product</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Project/Product</div>
+                </div>
+              </div>
+              <div className="column-3">
+                <div className="frame">
+                  <div className="text-wrapper-3">Domain</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Cse/Core</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Cse/Core</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Cse/Core</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Cse/Core</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Cse/Core</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Cse/Core</div>
+                </div>
+              </div>
+              <div className="column-4">
+                <div className="frame">
+                  <div className="text-wrapper-3">Faculty Guide</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Ms. Nandhini</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Ms. Nithya</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Ms. Poornima</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Mr. Somasundaram</div>
+                </div>
+                <div className="div-wrapper">
+                  <div className="text-wrapper-4">Ms. Divya</div>
+                </div>
+                <div className="frame-3">
+                  <div className="text-wrapper-4">Ms. Pousia</div>
+                </div>
+              </div>
+              <div className="column-5">
+                <div className="frame">
+                  <div className="text-wrapper-3">Provisional Approval</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Approved</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Approved</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Approved</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Rejected</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Rejected</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Rejected</div>
+                </div>
+              </div>
+              <div className="column-5">
+                <div className="frame">
+                  <div className="text-wrapper-3">Remarks</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Good</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Bad</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Good</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Bad</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Bad</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Bad</div>
+                </div>
+              </div>
+              <div className="column-6">
+                <div className="frame">
+                  <div className="text-wrapper-3">Tac Id</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">0001</div>
+                  <div className="frame-4">
+                    <div className="ellipse" />
+                    <div className="ellipse" />
+                    <div className="ellipse" />
+                  </div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">0002</div>
+                  <div className="frame-4">
+                    <div className="ellipse" />
+                    <div className="ellipse" />
+                    <div className="ellipse" />
+                  </div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">0003</div>
+                  <div className="frame-4">
+                    <div className="ellipse" />
+                    <div className="ellipse" />
+                    <div className="ellipse" />
+                  </div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">0004</div>
+                  <div className="frame-4">
+                    <div className="ellipse" />
+                    <div className="ellipse" />
+                    <div className="ellipse" />
+                  </div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-4">0005</div>
+                </div>
+                <div className="frame-2">
+                  <div className="text-wrapper-5">0006</div>
+                </div>
+              </div>
+            </div>
+            <img
+              className="svgrepo-iconcarrier"
+              alt="Svgrepo iconcarrier"
+              src="https://cdn.animaapp.com/projects/651cfb9e15319872b1327744/releases/651cfbde0b3bcd1fa07481c6/img/svgrepo-iconcarrier@2x.png"
+            />
+            <img
+              className="rectangle"
+              alt="Rectangle"
+              src="https://cdn.animaapp.com/projects/651cfb9e15319872b1327744/releases/651cfbde0b3bcd1fa07481c6/img/rectangle-55.svg"
+            />
+            <div className="group">
+              <div className="overlap-group-2">
+                <div className="apply-TAC">Apply&nbsp;&nbsp;TAC</div>
+                <div className="group-2">
+                  <img
+                    className="dashboard"
+                    alt="Dashboard"
+                    src="https://cdn.animaapp.com/projects/651cfb9e15319872b1327744/releases/651cfbde0b3bcd1fa07481c6/img/dashboard-1.png"
+                  />
+                  <div className="text-wrapper-6">Dashboard</div>
+                </div>
+                <div className="overlap-2">
+                  <div className="text-wrapper-7">TAC PORTAL</div>
+                </div>
+              </div>
+            </div>
+            <img
+              className="svgrepo-iconcarrier-2"
+              alt="Svgrepo iconcarrier"
+              src="https://cdn.animaapp.com/projects/651cfb9e15319872b1327744/releases/651cfbde0b3bcd1fa07481c6/img/svgrepo-iconcarrier@2x.png"
+            />
+          </div>
+          <img
+            className="ellipse-2"
+            alt="Ellipse"
+            src="https://cdn.animaapp.com/projects/651cfb9e15319872b1327744/releases/651cfbde0b3bcd1fa07481c6/img/ellipse-1@2x.png"
+          />
+          <div className="search-wrapper">
+            <img
+              className="search"
+              alt="Search"
+              src="https://cdn.animaapp.com/projects/651cfb9e15319872b1327744/releases/651cfbde0b3bcd1fa07481c6/img/search@2x.png"
+            />
+          </div>
+          <div className="rectangle-wrapper">
+            <div className="rectangle-2" />
+          </div>
+          <div className="overlap-wrapper">
+            <div className="overlap-3">
+              <div className="text-wrapper-8">Apply Tac</div>
+            </div>
+          </div>
+        </div>
+        <div className="group-3" />
+      </div>
+    </div>
+  );
+};
